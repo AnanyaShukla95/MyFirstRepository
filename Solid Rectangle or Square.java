fbe3e56775cd205ebd5446d1dd4c5c9b9a759255
AnanyaShukla95 <ananyashukla9595@gmail.com>
2025-09-13 11:48:20 +0530

    Add files via upload
    I HAVE ADDED SIMPLE PATTERN PRINTING PROGRAM

---
+++ Solid Rectangle or Square.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+	for(int r=1; r<5;r++){
+		for(int c=1; c<6;c++){
+			System.out.print("*");			
+	}
+	System.out.println();
+	}
+}
+}
